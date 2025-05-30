71f75c496912b97cdb5478e3bb55bd4440768f14
jsdomingo <53817072+jsdomingo@users.noreply.github.com>
2025-05-30 10:46:55 +1000

    feat: connecting searchBar to spotify search

--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,10 +1,30 @@
 import styles from './SearchBar.module.css'
+import { useState } from 'react'
 
-export default function SearchBar(){
+export default function SearchBar(props){
+    const [term, setTerm] = useState("");
+    
+    function passTerm() {
+        props.onSearch(term)
+    }
+
+    function handleTermChange({target}){
+        setTerm(target.value);
+    }
+
     return(
         <div className={styles.searchBar}>
-            <input className={styles.searchInput} placeholder='Enter Your Vibe' />
-            <button className={styles.searchBtn}>Search</button>
+            <input 
+            className={styles.searchInput} 
+            placeholder='Enter Your Vibe' 
+            onChange={handleTermChange}
+            />
+            <button 
+            className={styles.searchBtn}
+            onClick={passTerm}
+            >
+                Search
+            </button>
         </div>
     )
 }
