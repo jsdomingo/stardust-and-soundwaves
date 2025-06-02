d607a7712ab235640752a3d646bbdd1baa249746
jsdomingo <53817072+jsdomingo@users.noreply.github.com>
2025-06-02 16:57:49 +1000

    style: smoother transitions for the wave animation

--- src/WaveText/WaveText.jsx
+++ src/WaveText/WaveText.jsx
@@ -1,26 +1,22 @@
 import React, { useEffect, useRef } from "react";
-import anime from "animejs/lib/anime.es.js";
+import styles from '../WaveText/WaveText.module.css'  
+
 
 const WaveText = ({ text, className = "", delay = 0 }) => {
   const el = useRef(null);
 
   useEffect(() => {
-    const letters = el.current.querySelectorAll(".letter");
-
-    anime.timeline().add({
-      targets: letters,
-      translateY: [100, 0],
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 2000,
-      delay: (el, i) => delay + 60 * i,
+    const letters = el.current.querySelectorAll(`.${styles.letter}`);
+    letters.forEach((letter, i) => {
+      letter.style.setProperty("--delay", `${(delay + i * 60) / 1000}s`);
+      letter.classList.add(styles.animate);
     });
   }, [delay]);
 
   return (
     <h1 className={`title ${className}`} ref={el}>
       {text.split("").map((char, i) => (
-        <span className="letter" key={i}>
+        <span className={styles.letter} key={i}>
           {char}
         </span>
       ))}
